428cbb237b2623845ea8e84fa650b99b4c58f75e
Herry Tsai <herry@cruisys.com>
2025-06-05 21:22:44 +0800

    feat: generate content by prompt, last_node, next_node

---
+++ fastapi/node/const.py
@@ -0,0 +1,3 @@
+prompt_message_1 = '你是劇情產生器，請依照提示，生成劇情內容。提示: {user_prompt}'
+prompt_message_2 = '你是劇情產生器，請依照提示、前文，生成劇情內容。提示: {user_prompt}, 前文: {last_content}'
+prompt_message_3 = '你是劇情產生器，請依照提示、前文、後文，生成劇情內容。提示: {user_prompt}, 前文: {last_content}, 後文: {next_content}'
